1d2acc4da53d1b26f9690b38ebb7a8a263085692
aniruddha <aniruddha.kudalkar@gmail.com>
2016-06-03 13:17:18 +0530

    ui via xml

--- app/src/main/java/com/codekul/samplefirst/MainActivity.java
+++ app/src/main/java/com/codekul/samplefirst/MainActivity.java
@@ -1,69 +1,50 @@
 package com.codekul.samplefirst;
 
 import android.app.Activity;
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-public class MainActivity extends Activity {
 
-    public static final int ID_BTN_OKAY = 12345;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        LinearLayout layoutRoot = new LinearLayout(this);
-        layoutRoot.setOrientation(LinearLayout.VERTICAL);
-        layoutRoot.setLayoutParams(
-                new ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT));
+        final Click click = new Click();
+        setContentView(R.layout.activity_main);
 
-        EditText edtName = new EditText(this);
-        edtName.setHint("User Name");
-        edtName.setLayoutParams(
-                new ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT));
-        layoutRoot.addView(edtName);
+        Button btnUpper = (Button) findViewById(R.id.btnUpper);
+        btnUpper.setOnClickListener(click);
 
-        final Click click = new Click(edtName);
-
-        Button btnOkay = new Button(this);
-        btnOkay.setId(ID_BTN_OKAY);
-        btnOkay.setOnClickListener(click);
-        btnOkay.setText("Okay");
-        btnOkay.setLayoutParams(
-                new ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT));
-
-        layoutRoot.addView(btnOkay);
-
-        setContentView(layoutRoot);
+        Button btnLower = (Button) findViewById(R.id.btnLower);
+        btnLower.setOnClickListener(click);
+        Integer.parseInt("10");
     }
 
-    public class Click implements View.OnClickListener{
-
-        private final EditText edtName;
-
-        public Click(final EditText edtName){
-            this.edtName = edtName;
-        }
-
+    private class Click implements View.OnClickListener {
         @Override
         public void onClick(View v) {
 
-            if(v.getId() == ID_BTN_OKAY){
+            if(v.getId() == R.id.btnUpper) {
+                final EditText edtName = (EditText)
+                        findViewById(R.id.edtName);
 
-                String name = edtName.getText().toString();
+                String name = edtName.getText()
+                        .toString();
+
                 edtName.setText(name.toUpperCase());
+            }
+            else if(v.getId() == R.id.btnLower){
+
+                final EditText edtName = (EditText)
+                        findViewById(R.id.edtName);
+
+                String name = edtName.getText()
+                        .toString();
+
+                edtName.setText(name.toLowerCase());
             }
         }
     }
