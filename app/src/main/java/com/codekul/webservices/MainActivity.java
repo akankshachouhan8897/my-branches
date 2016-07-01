4d7da69089713ad470bee6ae2f3b433a9f39d75d
aniruddha <aniruddha.kudalkar@gmail.com>
2016-07-01 15:37:29 +0530

    web services

--- app/src/main/java/com/codekul/webservices/MainActivity.java
+++ app/src/main/java/com/codekul/webservices/MainActivity.java
@@ -1,12 +1,24 @@
 package com.codekul.webservices;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,93 +33,155 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                new MyTask().execute(/*params*/);
+                final EditText edtPostId =
+                        (EditText) findViewById(R.id.edtPostId);
+
+                new MyTask()
+                        .execute("http://jsonplaceholder.typicode.com/posts/"+edtPostId.getText()
+                                .toString());
             }
         });
     }
 
-    private void forOnMainThared(){
+    private class MyTask extends AsyncTask<String,Integer,String> {
 
-        final TextView textView =
-                (TextView) findViewById(R.id.textView);
-
-        for(int i = 0; i < 10 ;i++){
-
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            textView.setText(""+i);
-        }
-    }
-
-    private void forOnWorkerThread(){
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                final TextView textView =
-                        (TextView) findViewById(R.id.textView);
-
-                for(int i = 0; i < 10 ;i++){
-
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    textView.setText(""+i);
-                }
-            }
-        }).start();
-    }
-
-    private class MyTask extends AsyncTask<String/*params*/,Integer/*progress*/,Float/*Result*/> {
-
+        private ProgressDialog progressDialog;
         @Override
         protected void onPreExecute() {
 
-            // UI thread
+            progressDialog = ProgressDialog.show(MainActivity.this,
+                    "Post","Fetching Posts");
+
         }
 
         @Override
-        protected Float doInBackground(String... params/*params*/) {
-            // deals with worker thread
+        protected String doInBackground(String... params) {
 
-            for(int i = 0 ;i < 10; i++){
+            String json = "";
+            try {
+                URL url = new URL(params[0]);
 
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                HttpURLConnection connection =
+                        (HttpURLConnection) url.openConnection();
+
+                if(connection.getResponseCode() == 200){
+
+                    InputStream is =
+                            connection.getInputStream();
+
+                    StringBuilder builder =
+                            new StringBuilder();
+                    while(true){
+                        int ch = is.read();
+                        if(ch == -1) break;
+                        else builder.append((char)ch);
+                    }
+                    json = builder.toString();
                 }
 
-                publishProgress(new Integer[]{i/*progress*/});
-                // it will call onProgressUpdate
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            return null;
+            return json;
         }
 
         @Override
-        protected void onPostExecute(Float aFloat/*Result*/) {
-            super.onPostExecute(aFloat);
+        protected void onPostExecute(String str) {
+            super.onPostExecute(str);
 
+            progressDialog.dismiss();
 
-            //UI thread
+            Log.i("@codekul",str);
+
+            try {
+                JSONObject jsonObj = new JSONObject(str);
+                String postBody = jsonObj.getString("body");
+
+                final TextView textPost =
+                        (TextView) findViewById(R.id.textPost);
+                textPost.setText(postBody);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class MyPostTask extends AsyncTask<String,Integer,String> {
+
+        private ProgressDialog progressDialog;
+        @Override
+        protected void onPreExecute() {
+
+            progressDialog = ProgressDialog.show(MainActivity.this,
+                    "Post","Fetching Posts");
+
         }
 
         @Override
-        protected void onProgressUpdate(Integer... values/*progress*/) {
-            super.onProgressUpdate(values);
+        protected String doInBackground(String... params) {
 
-            final TextView textView =
-                    (TextView) findViewById(R.id.textView);
-            textView.setText(""+values[0]);
-            // UI thread
+            String json = "";
+            try {
+                URL url = new URL(params[0]);
+
+                HttpURLConnection connection =
+                        (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.getOutputStream()
+                        .write("{}".getBytes());
+                connection.getOutputStream()
+                        .flush();
+                connection.getOutputStream()
+                        .close();
+                connection.connect();
+
+                if(connection.getResponseCode() == 200){
+
+                    InputStream is =
+                            connection.getInputStream();
+
+                    StringBuilder builder =
+                            new StringBuilder();
+                    while(true){
+                        int ch = is.read();
+                        if(ch == -1) break;
+                        else builder.append((char)ch);
+                    }
+                    json = builder.toString();
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(String str) {
+            super.onPostExecute(str);
+
+            progressDialog.dismiss();
+
+            Log.i("@codekul",str);
+
+            try {
+                JSONObject jsonObj = new JSONObject(str);
+                String postBody = jsonObj.getString("body");
+
+                final TextView textPost =
+                        (TextView) findViewById(R.id.textPost);
+                textPost.setText(postBody);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
